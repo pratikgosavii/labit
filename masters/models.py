80dbf1ef1fd636aa5e220eba0661f146ccbd37dd
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-03-20 23:34:48 +0530

    Sk;kl

--- masters/models.py
+++ masters/models.py
@@ -68,6 +68,7 @@ from django.utils.timezone import now
 
 class coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)  # Unique coupon code
+    description = models.CharField(max_length=500, null=True, blank=True)  # Field for time slots
     discount_percentage = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)  # e.g., 10%
     discount_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)  # Max ₹1000 discount
     min_purchase = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # Min cart value required
