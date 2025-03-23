11e0b75ee99c7e48330be39a2635e5a7348f3690
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-03-24 05:08:58 +0530

    Sk;kl

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('signup/', SignupView.as_view(), name='signup'),
+    path('login-customer/', LoginView.as_view(), name='login'),
+    path('signup-customer/', SignupView.as_view(), name='signup'),
     path('logout/', logout_page, name='logout'),
     
     path('user_list/', user_list, name='user_list'),
